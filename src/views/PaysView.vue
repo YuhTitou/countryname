862cfe741d4b70272bdd50de77368f7345a6f0aa
YuhTitou <fichet1hugo@gmail.com>
2022-10-17 09:22:42 +0200

    yes

--- src/views/PaysView.vue
+++ src/views/PaysView.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="pays">
         <h2>Les Pays :</h2>
+        <input v-model="continent" placeholder="Nom de continent :">
+        <button v-on:click="filtre()">Filtre</button>
         <table>
             <tr>
                 <th>Nom</th>
                 <th>Drapeau</th>
             </tr>
+
             <tr v-for="(unpays,index) in this.resPays.name" :key="index">
                 <td>{{this.resPays.name[index]}}</td>
                 <router-link :to="{name : 'unpays', params : { id : index}}">
                     <td><img :src="this.resPays.url[index]"></td>
                 </router-link>
-                
             </tr>
 
         </table>
@@ -27,9 +29,12 @@ export default {
     return {
         resPays:{
             url:[],
-            name:[]
+            name:[],
+            continents:[]
         },
         pays:[],
+        result:[],
+        continent:""
         
     }
     },
@@ -44,12 +49,40 @@ export default {
                 for (let index = 0; index < this.pays.length; index++) {
                     this.resPays.url.push(this.pays[index]["flags"]["png"]);
                     this.resPays.name.push(this.pays[index]["translations"]["fra"]["common"]);
+                    this.resPays.continents.push(this.pays[index]["continents"][0])
                     
                 }
-                console.log(this.resPays.name)
-                console.log(this.resPays.url)
+                console.log(this.resPays.continents)
+                
+                
             });
         },
+        filtre(){
+            this.resPays.url = []
+            this.resPays.name = []
+            this.resPays.continents= []
+            axios.get("https://restcountries.com/v3.1/all").then((response) => {
+                console.log(response.data)
+                this.pays=response.data
+                for (let index = 0; index < this.pays.length; index++) {
+                    if(this.continent == this.pays[index]["continents"][0]){
+                        this.resPays.url.push(this.pays[index]["flags"]["png"]);
+                        this.resPays.name.push(this.pays[index]["translations"]["fra"]["common"]);
+                        this.resPays.continents.push(this.pays[index]["continents"][0])
+                    }
+                    else if(this.continent == ""){
+                        this.resPays.url.push(this.pays[index]["flags"]["png"]);
+                        this.resPays.name.push(this.pays[index]["translations"]["fra"]["common"]);
+                        this.resPays.continents.push(this.pays[index]["continents"][0])
+                    }
+                    
+                    
+                }
+                console.log(this.resPays)
+                
+                
+            });
+        }
     }
 }
 </script>
@@ -84,5 +117,18 @@ div{
     justify-content: center;
     flex-direction: column;
     align-items: center;
+}
+input{
+    font-size: large;
+    padding: 10px;
+    
+}
+button{
+  background-color: #42b983;
+  color: #2c3e50;
+  padding: 10px;
+  margin: 10px;
+  font-size: larger;
+  border-radius: 5px;
 }
 </style>
